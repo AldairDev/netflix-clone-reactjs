c60c2834c044f870fe29ff8a6ecee70ded9f375d
aldair sosa <aldairsosa18@gmail.com>
2020-04-03 21:41:25 -0500

    solving import images from the public

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Index } from './pages/Index'
+
+export const App = () => {
+
+  return (
+    <div className="App">
+      <Index />
+      {/* <img src="/images/fb-logo.png" alt="prueba"/> */}
+    </div>
+  );
+}
+
---
+++ src/pages/Index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../layouts/Header'
+import '../css/index.css';
+// import '/css/index.css'
+export const Index = () => {
+
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
