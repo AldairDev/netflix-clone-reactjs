07f8621e1215ebf398baeef5fee9d4dd0a4008bb
aldair sosa <aldairsosa18@gmail.com>
2020-04-16 13:30:57 -0500

    Updates

---
+++ precache-manifest.637973a15ec0d7f984bd74bb5c2c91ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5e8e99d65881758fd50e14da8b8d64",
+    "url": "/netflix-clone-reactjs/index.html"
+  },
+  {
+    "revision": "a3fedc4f0f72b4be6868",
+    "url": "/netflix-clone-reactjs/static/css/main.cacaea9f.chunk.css"
+  },
+  {
+    "revision": "f1782ed1f03e68e05ed4",
+    "url": "/netflix-clone-reactjs/static/js/2.8cc4dca1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-clone-reactjs/static/js/2.8cc4dca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3fedc4f0f72b4be6868",
+    "url": "/netflix-clone-reactjs/static/js/main.bb2ee4c2.chunk.js"
+  },
+  {
+    "revision": "6d1890db146feb25eb05",
+    "url": "/netflix-clone-reactjs/static/js/runtime-main.a894e599.js"
+  },
+  {
+    "revision": "dc2411c98b969c2d71b0920ae3e2d9d8",
+    "url": "/netflix-clone-reactjs/static/media/bg.dc2411c9.jpg"
+  },
+  {
+    "revision": "a04ae0abeac56773dd82d15a7a9f9f8c",
+    "url": "/netflix-clone-reactjs/static/media/cross-out.a04ae0ab.svg"
+  },
+  {
+    "revision": "35f3a5aec6a2026d1f96de322219136d",
+    "url": "/netflix-clone-reactjs/static/media/logo.35f3a5ae.svg"
+  },
+  {
+    "revision": "855f328d6bb1b68cdae0428efb2536d7",
+    "url": "/netflix-clone-reactjs/static/media/right.855f328d.svg"
+  },
+  {
+    "revision": "77994a67327ba957dfd880e33a91f041",
+    "url": "/netflix-clone-reactjs/static/media/tv.77994a67.png"
+  }
+]);
