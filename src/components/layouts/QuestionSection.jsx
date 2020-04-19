8fa0e66dfacedf9471163cde146baedb954523a9
aldair sosa <aldairsosa18@gmail.com>
2020-04-19 02:19:16 -0500

    [Added] iconQuestion in question section]

--- src/components/layouts/QuestionSection.jsx
+++ src/components/layouts/QuestionSection.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Index as FormInput } from "../form-input/Index";
 
-import arrow from "../../assets/svg/cross-out.svg";
+import IconClose from "../../assets/svg/cross-out.svg";
 
 const QuestionSection = ({ data }) => {
   const [showAnwser, setShowAnwser] = useState([]);
-
+  // const [prueba, setPrueba] = useState(true)
   useEffect(() => {
     setShowAnwser(data);
   }, [data]);
@@ -18,6 +18,8 @@ const QuestionSection = ({ data }) => {
       )
     );
   };
+  console.log(showAnwser);
+
   return (
     <WraperQuestion>
       <h1>Preguntas Frecuentes </h1>
@@ -27,6 +29,11 @@ const QuestionSection = ({ data }) => {
             <div>
               <button className="button-question" onClick={() => openAnswer(i)}>
                 {question.title}
+                {question.visibility === true ? (
+                  <img src={IconClose} alt="iconClose" className="close-icon" />
+                ) : (
+                  <img src={IconClose} alt="iconClose" className="open-icon" />
+                )}
               </button>
             </div>
             {question.visibility === true && (
@@ -74,8 +81,27 @@ const WraperQuestion = styled.div`
     justify-content: center;
     margin-top: -1.2rem;
   }
+  .close-icon{
+    max-height: 0.70em;
+    position: absolute;
+    right: 1.5em;
+    transform: translateX(50%) rotate(-90deg);
+    transition: 0.2s;
+    bottom: 37%;
+  }
+  .open-icon{
+    max-height: 0.70em;
+    position: absolute;
+    margin: auto 0;
+    bottom: 37%;
+    right: 1.5em;
+    transition: 0.2s;
+    transform: translateX(50%) rotate(45deg);
+
+  }
 
   .button-question {
+    position: relative;
     text-decoration: none;
     font-size: 1.6rem;
     text-align: left;
